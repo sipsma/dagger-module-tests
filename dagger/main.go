6f1c552c151739df3e3b8afcf8d61e3a8228b2b5
Erik Sipsma <erik@sipsma.dev>
2024-02-19 18:04:13 -0800

    add module to root of repo
    Signed-off-by: Erik Sipsma <erik@sipsma.dev>

---
+++ dagger/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+)
+
+type RootMod struct {}
+
+// example usage: "dagger call container-echo --string-arg yo stdout"
+func (m *RootMod) ContainerEcho(stringArg string) *Container {
+	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg})
+}
+
+// example usage: "dagger call grep-dir --directory-arg . --pattern GrepDir"
+func (m *RootMod) GrepDir(ctx context.Context, directoryArg *Directory, pattern string) (string, error) {
+	return dag.Container().
+		From("alpine:latest").
+		WithMountedDirectory("/mnt", directoryArg).
+		WithWorkdir("/mnt").
+		WithExec([]string{"grep", "-R", pattern, "."}).
+		Stdout(ctx)
+}
+
